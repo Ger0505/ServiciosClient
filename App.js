ca5c80dfd4cfaa020746952865355736a19bc9e2
Francisco Árciga Nolasco <Francisco@Kribrick.local>
2021-03-16 11:17:31 -0600

    Pull

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import * as Components from "native-base";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
+const [Container, Text] = [Components.Container, Components.Text]; 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isReady: false,
+      isReady: false
     };
   }
 
   async componentDidMount() {
     await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
     });
     this.setState({ isReady: true });
   }
